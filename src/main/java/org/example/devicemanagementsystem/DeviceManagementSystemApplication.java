0f12947359d22c2bd1ac5e6303c185a2d6d55c56
Kartik Badiger <kartik.badiger@setra.com>
2026-01-14 15:25:24 +0530

    Initial commit: device-management-system

---
+++ src/main/java/org/example/devicemanagementsystem/DeviceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.devicemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/devicemanagementsystem/DeviceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devicemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
